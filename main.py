c40647dd1336cc1eb5f596b4a6a94e4e0cbbb602
Joseph Glasson <josephglasson@Josephs-MacBook-Pro-27.local>
2026-02-18 15:25:49 -0500

    Fit logistic regression model

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from data_prep import load_and_clean_data
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+import pandas as pd
+import numpy as np
 
 # load processed data
 X, y = load_and_clean_data()
@@ -21,10 +23,20 @@ print("Test shape:", X_test.shape)
 
 
 model = Pipeline([
-    ("scale", StandardScaler()),
-    ("lr", LogisticRegression(max_iter=2000))
+    ("scale", StandardScaler()), #standardise values
+    ("lr", LogisticRegression(max_iter=2000)) #predicts probabilities for a binary outcome
 ])
 
 model.fit(X_train, y_train)
-print("Train accuracy:", model.score(X_train, y_train))
-print("Test accuracy:", model.score(X_test, y_test))
+print("Train accuracy:", model.score(X_train, y_train)) # ~ 0.911
+print("Test accuracy:", model.score(X_test, y_test)) # ~ 0.839
+# not overfitting
+
+
+
+#rank IVs by importance
+coefs = model.named_steps["lr"].coef_[0] #stores coeffs from fitted logistic regression model
+importance = pd.Series(np.abs(coefs), index=X.columns).sort_values(ascending=False) #take absolute value of coefs and display as labeled Pandas series, sorted descending order
+print(importance.head(10))
+# all predictors are standardised --> comparing coeffs is valid
+
